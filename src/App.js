7533ceb55102442deed4a6d4e364d4d9fa64af01
Crystal Chen <cry_fairy@crystaldembp.home>
2022-10-03 21:50:13 -0400

    finished lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
 
-function App() {
+export default class App extends Component {
+  state = {
+    level: 1,
+    status: 'Nice',
+    statusColor: 'black'
+  };
+  updateLevel = (incomingLevel) => {
+    let newLevel = this.state.level;
+    let newStatus = this.state.status;
+    let newStatusColor = this.state.statusColor
+    newLevel += incomingLevel
+    if (newLevel < 1 || newLevel > 25) {
+      return;
+    }
+
+    if (newLevel <= 2) {
+      newStatus = "Nice";
+      newStatusColor = "black";
+    } else if (newLevel <= 4) {
+      newStatus = "Start mopping your brow";
+      newStatusColor = "green";
+    } else if (newLevel <= 9) {
+      newStatus = "Watch out";
+      newStatusColor = "pink";
+    } else if (newLevel <= 14) {
+      newStatus = "Are you sure?";
+      newStatusColor = "orange";
+    } else if (newLevel <= 19) {
+      newStatus = "Some like it hot";
+      newStatusColor = "brown";
+    } else if (newLevel <= 25) {
+      newStatus = "May cause stomach upset";
+      newStatusColor = "red";
+    } 
+    this.setState(
+      {
+        level:newLevel,
+        status: newStatus,
+        statusColor: newStatusColor,
+      }
+    );
+  }
+
+
+  
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>🌶️ Salad King Pepper Chart SPA</h1>
+      <button onClick={() => this.updateLevel(1)}>Add Some Heat</button>
+      <button onClick={() => this.updateLevel(-1)}>Cool It Off</button>
+      <h2 >
+        Spice Level:{this.state.level}
+      </h2>
+      <h2 style={{color: this.state.statusColor}}>
+        Status: {this.state.status}
+      </h2>
     </div>
-  );
+    );
+  }
 }
 
-export default App;
+
+
